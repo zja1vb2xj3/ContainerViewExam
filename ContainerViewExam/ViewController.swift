4eb81de99f3df9c5b37fce7be293431e383a5174
Beaconyx Corp <beaconyxcorp.@beaconyxui-macbook-pro.local>
2018-02-08 15:23:50 +0900

    Initial Commit

---
+++ ContainerViewExam/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ContainerViewExam
+//
+//  Created by Beaconyx Corp. on 2018. 2. 8..
+//  Copyright © 2018년 Beaconyx Corp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
